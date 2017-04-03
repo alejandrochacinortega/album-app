ef673f4f9742881d821ae31d16836e52011f9ac1
alejandro <alejandro@zeppelin.no>
2017-04-03 18:51:11 +0200

    first commit

---
+++ index.ios.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import Header from './src/components/Header';
+
+export default class albums extends Component {
+  render() {
+    return (
+      <Header/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('albums', () => albums);
