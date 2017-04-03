bbb4767014ac93d02ab2033e17448e03acfdfbdf
alejandro <alejandro@zeppelin.no>
2017-04-03 21:27:05 +0200

    add Card component

--- index.ios.js
+++ index.ios.js
@@ -7,14 +7,19 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  View
 } from 'react-native';
 
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 export default class albums extends Component {
   render() {
     return (
-      <Header headerText={"Albums"}/>
+      <View>
+        <Header headerText={"Albums"}/>
+        <AlbumList/>
+      </View>
     );
   }
 }
---
+++ src/components/AlbumList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  state = {
+    albums: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map((album,index) => {
+      return (
+        <AlbumDetail key={index} album={album}/>
+      );
+    });
+  }
+
+  render() {
+    console.log(' rendering ', this.state.albums);
+    if (this.state.albums.length === 0) {
+      return <Text>Loading...</Text>;
+    }
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
